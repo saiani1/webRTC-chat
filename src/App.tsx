7011a5c9c87588d307540c5b76530506912dbf67
saiani1 <saiani@naver.com>
2023-10-25 15:12:16 +0900

    localDescription, remoteDescription설정 로직 구현(수정필요)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,57 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [localDes, setLocalDes] = useState({
+    type: "answer",
+    sdp: "",
+  });
+
+  const setRemoteRef = useRef<HTMLInputElement>(null);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  const RTCPeerConnctionObject = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  });
+
+  const handleSDPBtnClick = async () => {
+    const offer = await RTCPeerConnctionObject.createOffer({
+      iceRestart: true,
+    });
+    await RTCPeerConnctionObject.setLocalDescription(offer);
+    if (offer.sdp) setLocalDes({ ...localDes, sdp: offer.sdp });
+  };
+
+  const handleSetRemoteBtnClick = async () => {
+    const ref = setRemoteRef.current;
+    const sdp = ref?.value;
+    if (ref && RTCPeerConnctionObject && sdp) {
+      await RTCPeerConnctionObject.setRemoteDescription(
+        new RTCSessionDescription(JSON.parse(sdp))
+      );
+      console.log(RTCPeerConnctionObject);
+    }
+  };
+
   return (
     <div className="wrap">
-      <form className="inputWrap">
+      <div className="inputWrap">
         <label htmlFor="sdp">SDP</label>
-        <input type="text" id="sdp" />
-        <button type="submit">생성</button>
-      </form>
-      <form className="inputWrap">
+        <p>{JSON.stringify(localDes)}</p>
+        <button type="button" onClick={handleSDPBtnClick}>
+          생성
+        </button>
+      </div>
+      <div className="inputWrap">
         <label htmlFor="setRemote">setRemote</label>
-        <input type="text" id="setRemote" />
-        <button type="submit">설정</button>
-      </form>
+        <input type="text" id="setRemote" ref={setRemoteRef} />
+        <button type="button" onClick={handleSetRemoteBtnClick}>
+          설정
+        </button>
+      </div>
       <div className="inputWrap">
         <label htmlFor="chat">chat</label>
-        <textarea />
+        <textarea ref={textRef} />
       </div>
     </div>
   );
