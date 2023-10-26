fc81c4803b8d44335828c748bcb1725b7dafb9a2
saiani1 <saiani@naver.com>
2023-10-26 13:05:36 +0900

    나머지 로직 작성 (수정필요)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,86 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./App.css";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 function App() {
   const [localDes, setLocalDes] = useState({
     type: "answer",
     sdp: "",
   });
-
+  const [isSendOffer, setIsSendOffer] = useState(false);
+  const [chat, setChat] = useState("");
+  const textRef = useRef<HTMLInputElement>(null);
   const setRemoteRef = useRef<HTMLInputElement>(null);
-  const textRef = useRef<HTMLTextAreaElement>(null);
 
-  const RTCPeerConnctionObject = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-  });
+  const peer = new RTCPeerConnection();
+  let dc = peer.createDataChannel("channel");
+  peer.onicecandidate = () => {};
 
   const handleSDPBtnClick = async () => {
-    const offer = await RTCPeerConnctionObject.createOffer({
-      iceRestart: true,
-    });
-    await RTCPeerConnctionObject.setLocalDescription(offer);
-    if (offer.sdp) setLocalDes({ ...localDes, sdp: offer.sdp });
+    peer
+      .createOffer()
+      .then((o) =>
+        peer.setLocalDescription(o).then(() => {
+          toast.success("성공적으로 localDescription을 설정했어요.");
+          if (peer.localDescription?.sdp) {
+            setLocalDes({
+              ...localDes,
+              sdp: peer.localDescription?.sdp,
+            });
+            setIsSendOffer(true);
+          }
+        })
+      )
+      .catch((err) => toast.error(err));
   };
 
-  const handleSetRemoteBtnClick = async () => {
+  const handleSetRemoteBtnClick = () => {
     const ref = setRemoteRef.current;
     const sdp = ref?.value;
-    if (ref && RTCPeerConnctionObject && sdp) {
-      await RTCPeerConnctionObject.setRemoteDescription(
-        new RTCSessionDescription(JSON.parse(sdp))
-      );
-      console.log(RTCPeerConnctionObject);
+
+    if (!isSendOffer) {
+      toast.success("채널을 생성할게요.");
+      peer.ondatachannel = (e) => {
+        dc = e.channel;
+        dc.onmessage = (e) => {
+          toast.success("새로운 메시지가 도착했어요!");
+          setChat(e.data);
+        };
+        dc.onopen = () => {
+          toast.success("상대방과 연결되었습니다. 채팅을 시작할 수 있습니다.");
+        };
+      };
+    }
+
+    if (ref && sdp) {
+      peer
+        .setRemoteDescription(new RTCSessionDescription(JSON.parse(sdp)))
+        .then(() => toast.success("RemoteDescription을 설정했어요!"));
+      if (localDes.sdp !== "" && !isSendOffer)
+        peer.createAnswer().then((a) =>
+          peer.setLocalDescription(a).then(() => {
+            toast.success("answer를 생성했어요");
+            if (peer.localDescription?.sdp) {
+              setLocalDes({ ...localDes, sdp: peer.localDescription?.sdp });
+            }
+          })
+        );
     }
   };
+
+  const handleChatArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setChat((prev) => prev + e.target.value);
+  };
+
+  const handleSendText = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const ref = textRef.current;
+    const text = ref?.value;
+    if (text) dc.send(text);
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <div className="wrap">
@@ -49,10 +98,22 @@ function App() {
           설정
         </button>
       </div>
+      <form className="inputWrap" onSubmit={handleSendText}>
+        <label htmlFor="enteredText">텍스트 입력</label>
+        <input type="text" id="enteredText" ref={textRef} />
+      </form>
       <div className="inputWrap">
         <label htmlFor="chat">chat</label>
-        <textarea ref={textRef} />
+        <textarea value={chat} onChange={handleChatArea} />
       </div>
+      <Toaster
+        containerStyle={{
+          top: 20,
+        }}
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
     </div>
   );
 }
